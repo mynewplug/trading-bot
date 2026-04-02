c01e2260035f34331dc7917035607a2ca2503e64
mynewplug <89512442+mynewplug@users.noreply.github.com>
2026-04-02 19:35:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -144,8 +144,6 @@ def webhook():
         status, response_text = place_order(symbol, action, sl, tp)
 
         return jsonify({
-Use Control + Shift + m to toggle the tab key moving focus. Alternatively, use esc then tab to move to the next interactive element on the page.
-
             "ok": status in [200, 201],
             "status": "executed" if status in [200, 201] else "failed",
             "oanda_status": status,
